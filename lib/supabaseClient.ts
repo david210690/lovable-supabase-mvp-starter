29936373e384159da59e28d35044792dc95d4d44
david210690 <98743423+david210690@users.noreply.github.com>
2025-10-04 01:07:02 +0530

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,10 +1,7 @@
-// lib/supabaseClient.ts
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL as string,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string,
-  {
-    auth: { persistSession: true, autoRefreshToken: true }
-  }
-)
+const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+
+// Create a single browser/server client for interacting with your database
+export const supabase = createClient(url, anon);
